f7080d6ba768489403972d248fc03f6839b0a063
Kaser81 <s.karablinn@gmail.com>
2022-02-05 23:36:44 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPtice = 2500;
+        int partTicket = 20;
+        int miles = ticketPtice / partTicket;
+        System.out.println("Количество начисленных миль за купленный билет: " + miles);
+    }
+}
